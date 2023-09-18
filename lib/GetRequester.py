68971c2fd6cf0df931444e86d173a7456c6cd530
Bisinle <Abdiwadud.mohamedd@gmail.com>
2023-09-18 19:38:02 +0300

    passed all the tests

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,13 +1,28 @@
 import requests
 import json
 
+# class GetRequester:
+
+#     def __init__(self, url):
+#         self.url = url
+
+#     def get_response_body(self):
+#         pass
+
+#     def load_json(self):
+#         pass
+
+
 class GetRequester:
-
     def __init__(self, url):
-        self.url = url
+        self.url=url
 
+    # ge the response body 
     def get_response_body(self):
-        pass
+        return requests.get(self.url).content
 
+        
+    # use get_response_body, load it and return a python list of dict
     def load_json(self):
-        pass
+        json_content = json.loads(self.get_response_body())
+        return json_content
